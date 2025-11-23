fd36db6d9e51555f0d1b34ea3360a7f091f1bbdf
Matt <mattymeans@gmail.com>
2025-11-22 22:41:46 -0700

    Added css and additional values to picsearch

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env.LOCAL' });
+
 import express from "express";
 import bodyParser from "body-parser";
 import cors from 'cors'
@@ -5,15 +8,16 @@ import { createClient } from "pexels";
 
 const app = express();
 const port = 3000;
-const client = createClient('Pl4TeM4NLHm9o7aHo7k5hkiTVD5m2E3JYqegCXkh4ZgCiwVuZfNBUeIq')
+const client = createClient(process.env.API_KEY)
 app.use(express.static('public'));
 
 app.get("/", async (req, res) =>{
     res.render("main.ejs")
 })
-app.get("/maker.ejs", async (req,res) =>{
-    const picsRequest = await client.photos.curated({page: 1, per_page: 40});
-    res.render("maker.ejs", {picResults: picsRequest.photos});
+app.get("/maker", async (req,res) =>{
+    const picsRequest = await client.photos.curated({page: 1, per_page: 20});
+    console.log(picsRequest.next_page);
+    res.render("maker.ejs", {picResults: picsRequest.photos, nextpage: picsRequest.next_page, prevpage: picsRequest.prev_page, currentpage: picsRequest.page});
 })
 
 
